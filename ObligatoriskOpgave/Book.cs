d26fc0b62516e9c311f8a667b29e55520ca38134
Omar Jaber <48085067+Ojay89@users.noreply.github.com>
2020-02-27 09:30:47 +0100

    Add project files.

---
+++ ObligatoriskOpgave/Book.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace ObligatoriskOpgave
+{
+    public class Book
+    {
+        //Instance Field
+        private string _title;
+        private string _author;
+        private int _pageNo;
+        private long _isbn13;
+
+        //Constructor
+        public Book(string Title, string Author, int PageNo, long Isbn13)
+        {
+            _title = Title;
+            _author = Author;
+            _pageNo = PageNo;
+            _isbn13 = Isbn13;
+        }
+
+        //Properties
+        public string Title
+        {
+            get => _title;
+            set { _title = value; }
+        }
+
+        public string Author
+        {
+            get => _author;
+            set
+            {
+                if (value == null) throw new ArgumentException("Skriv et navn");
+                if (value.Length < 2) throw new ArgumentException("Forfatter navn skal være minimum 2 tegn langt");
+                _title = value;
+            }
+
+        }
+
+        public int PageNo
+        {
+            get => _pageNo;
+            set
+            {
+                if (value <= 4) throw new ArgumentException("Bogen er for kort");
+                if (value >= 1000) throw new ArgumentException("Bogen er for lang");
+            }
+        }
+
+        //public long Isbn13
+        //{
+        //    get => _isbn13;
+
+        //    set
+
+        //    {
+        //        _isbn13 = value;
+        //        if (value <= 12) throw new ArgumentOutOfRangeException("ISBN", value, "Er for kort. Skal være 13 cifre.");
+        //        else if (value >= 14) throw new ArgumentOutOfRangeException("ISBN", value, "ISBN er for lang. Skal være 13 cifre");
+        //        if (value == 13) _isbn13 = value;
+
+        //    }
+        //}
+    }
+}
---
+++ ObligatoriskOpgaveTests/BookTests.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ObligatoriskOpgave;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace BookTest
+{
+    [TestClass()]
+    public class BookTests
+    {
+        private Book _book;
+
+        [TestInitialize]
+        public void BookTest()
+        {
+            _book = new Book(Title: "Omars bog", Author: "Omar Jaber", 498, 1123145234123); //arrange
+        }
+
+        [TestMethod()]
+        public void TitleTest()
+        {
+            _book.Title = "Omars bog"; //act
+            Assert.AreEqual("Omars bog", _book.Title); // Assert
+        }
+
+        [TestMethod()]
+        public void AuthorTest()
+        {
+            _book.Author = "Omar Jaber"; //act
+            Assert.AreEqual("Omar Jaber", _book.Author); //Assert
+        }
+
+        [TestMethod()]
+        public void PageNoTest()
+        {
+            _book.PageNo = 498; //act
+            Assert.AreEqual(498, _book.PageNo); //Assert
+        }
+
+        //[TestMethod()]
+        //public void Isbn13Test()
+        //{
+        //    _book.Isbn13 = 9371947264912; //act
+        //    Assert.AreEqual(9371947264912, _book.Isbn13); //Assert
+        //}
+
+
+    }
+}
+
+namespace ObligatoriskOpgaveTests
+{
+    class BookTests
+    {
+    }
+}
